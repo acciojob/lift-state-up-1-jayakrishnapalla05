87019c9db5d2d162d24a12558dc3c14b47dd1d79
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-15 12:56:25 +0530

    Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, {useState} from "react";
+import Child from "./Child";
 import './../styles/App.css';
 
 const App = () => {
+  const [modal, showModal] = useState(false);
+  const updateParent = () => {
+    showModal(true);
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <Child updateParent={updateParent} modal={modal} />
     </div>
   )
 }
